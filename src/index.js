41b94fae2202b96630a62bf99080d625a7057cf2
Hanna Dozhyna <annadozhyna@gmail.com>
2019-10-08 15:25:32 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+  let factorials = expression.split("*");
+  let doubleFact = [];
+  let onceFact = [];
+
+  let fives = 0;
+  let deuces = 0;
+  for (let i = 0; i < factorials.length; i++) {
+    if (factorials[i].includes("!!")) {
+      doubleFact.push(factorials[i].slice(0, -2));
+    } else {
+      onceFact.push(factorials[i].slice(0, -1));
+    }
+  }
+
+  for (let i = 0; i < onceFact.length; i++) {
+    let number = onceFact[i];
+    for (let j = 5; j <= number; j *= 5) {
+      fives += Math.floor(number / j);
+    }
+
+    for (let j = 2; j <= number; j *= 2) {
+      deuces += Math.floor(number / j);
+    }
+  }
+
+  for (let i = 0; i < doubleFact.length; i++) {
+    number = doubleFact[i];
+    for (let j = 5; j <= number; j *= 5) {
+      if (number % 2 !== 0) {
+        fives += Math.ceil(Math.floor(number / j) / 2);
+      } else {
+        fives += Math.floor(Math.floor(number / j) / 2);
+      }
+    }
+    for (let j = 2; j <= number; j *= 2) {
+      if (number % 2 == 0) {
+        deuces += Math.floor(number / 2);
+      } else {
+        deuces += 0;
+      }
+    }
+  }
+  return fives > deuces ? deuces : fives;
+};
